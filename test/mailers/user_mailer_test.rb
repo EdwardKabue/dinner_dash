d314158ad91477ddba40781d490bab339c1d589d
Edward <kabueedward@yahoo.com>
2015-04-26 18:49:38 +0300

    Defined a unit test for the mailer

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
   test "new_password" do
-    mail = UserMailer.new_password
-    assert_equal "New password", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    mail = UserMailer.new_password(users(:edward), "lambasted")
+    assert_equal "Your new password", mail.subject
+    assert_equal ["#{users(:edward).email}"], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match "Your new password is: lambasted", mail.body.encoded
   end
 
 end
